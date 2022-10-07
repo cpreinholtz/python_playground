bf2196622e89a7a5f603f73855d08dddcd8a2c55
cpreinholtz <cpr978@gmail.com>
2022-10-06 21:23:56 -0700

    basic functionality is working

--- timecard/timecard.py
+++ timecard/timecard.py
@@ -10,28 +10,29 @@ gWorking = False
 gDf = False
 
 #*********************************888
-def main():
-    debug('hello, this program is a timecard')
-    global gDf
-    gDf = createNew()
 
-    
+def init():
+
+    readCsvFile()
+    recalculateDiff()
+    global gDf    
+
     debug(gDf.to_string())
 
-    run()
-    run()
-    run()
 
+def main():
+    debug('hello, this program is a timecard')
+    init()
+
+    reportThisWeek()
     while True:
-        try:
-            run()
-        except:
-            break
-
+        run()
 
 def run():
     global gWorking
     if gWorking == True:
+        global gDf
+        msg('you are working on' + gDf.at[len(gDf.index)-1, 'Proj'])
         input('press enter to clock OUT')
         clockOut()
     else:
@@ -41,13 +42,78 @@ def run():
 
 
 
+def reportThisWeek(start_date = datetime.date.today(), end_date = datetime.date.today()+ datetime.timedelta(days = 1)):
+    start_date = datetime.date.today()
+    for x in range(6):
+        start_date = datetime.date.today() - datetime.timedelta(days = x)
+        end_date = start_date + datetime.timedelta(days = 1)
+        msg('******************')
+        report(start_date =start_date, end_date =  end_date)
+    msg('******************')
 
 #*********************************888
-#project, in time, out time
-def createNew( data = [['Dev', datetime.datetime.now(), datetime.datetime.now()+datetime.timedelta(hours = 1)]] ):
-    columns = ['Proj', 'In', 'Out']
+#%d/%m/%Y
+def report(start_date = datetime.date.today(), end_date = datetime.date.today()+ datetime.timedelta(days = 1)):
+
+
+
+    start_datetime = pd.to_datetime(start_date, format = "%Y-%m-%d")
+    end_datetime = pd.to_datetime(end_date,  format = "%Y-%m-%d")
+
+    msg('from ' + str(start_datetime) +' to ' + str(end_datetime) + ' you worked these hours:')
+    global gDf
+
+    inDatetimes = pd.to_datetime(gDf.loc[:,'In'], format = "%Y-%m-%d %H:%M:%S")
+    outDatetimes = pd.to_datetime(gDf.loc[:,'Out'], format = "%Y-%m-%d %H:%M:%S")
+    mask = ( inDatetimes > start_datetime) & (inDatetimes <= end_datetime)
+    if any(mask):
+        df2 = gDf.loc[mask]
+        debug(df2)
+        dff = df2.groupby(["Proj"]).Diff.sum().reset_index()
+        msg(dff)
+    else:
+        msg('No work from ' + str(start_datetime) +' to ' + str(end_datetime))
+
+
+
+def recalculateDiff():
+    global gDf
+    inDatetimes = pd.to_datetime(gDf.loc[:,'In'], format = "%Y-%m-%d %H:%M:%S")
+    outDatetimes = pd.to_datetime(gDf.loc[:,'Out'], format = "%Y-%m-%d %H:%M:%S")
+    gDf['Diff'] = outDatetimes - inDatetimes
+    writeCsvFile()
+
+#*********************************888
+def createNew( data = [['Dev', None, None, None]] ):
+    columns = ['Proj', 'In', 'Out', 'Diff']
     return pd.DataFrame(data, columns = columns)
 
+
+def readCsvFile(fileName = './card.csv'):
+    fileName = './card.csv'
+    global gDf
+
+    try:
+        gDf = pd.read_csv(fileName, index_col=0)
+    except:
+        gDf = createNew()
+        writeCsvFile()
+
+    #if gDf.at[len(gDf.index)-1, 'Out'] == None or gDf.at[len(gDf.index)-1, 'Out'] == nan:
+    if pd.isnull(gDf.at[len(gDf.index)-1, 'Out']) and pd.notnull(gDf.at[len(gDf.index)-1, 'In']):
+        global gWorking
+        gWorking = True
+        debug('no clockout detected on last row')
+        debug(gDf.at[len(gDf.index)-1, 'Out'])
+    else:
+        gWorking = False
+        debug('clockout detected in last csv ')
+        debug(gDf.at[len(gDf.index)-1, 'Out'])
+
+
+def writeCsvFile(fileName = './card.csv'):
+    global gDf
+    gDf.to_csv(fileName)
 
 
 
@@ -62,10 +128,11 @@ def clockIn(project ='NA' ):
         err(gWorking)
     gWorking = True
     addRow(project = project, inTime = tTime)
+    writeCsvFile()
 
-def addRow(project = 'NA' , inTime = datetime.datetime.now(), outTime = None):
+def addRow(project = 'NA' , inTime = datetime.datetime.now(), outTime = None,  diff = None):
     global gDf
-    gDf = pd.concat( [gDf, createNew(data = [[project, inTime, outTime]]) ], ignore_index=True)
+    gDf = pd.concat( [gDf, createNew(data = [[project, inTime, outTime, diff]]) ], ignore_index=True)
 
 
 #*********************************888
@@ -78,19 +145,24 @@ def clockOut( ):
         err('double clock out detected')
     gWorking = False
     updateLastOut(tTime)
+    writeCsvFile()
 
 def updateLastOut(outTime):
     global gDf
     debug(len(gDf.index))
     debug(gDf.at[0, 'Out'])
+    inTime = pd.to_datetime(gDf.at[len(gDf.index)-1, 'In'] , format = "%Y-%m-%d %H:%M:%S")
     gDf.at[len(gDf.index)-1, 'Out'] = outTime
+    gDf.at[len(gDf.index)-1, 'Diff'] = outTime - inTime
+
 
 
 
 #*********************************888
 def debug(str =""):
     print(str, flush=True)
-
+def msg(str = ''):
+    print(str, flush=True)
 def err(str =""):
     print("**ERROR DETECTED*******************", flush=True)
     print(str, flush=True)
